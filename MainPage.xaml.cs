5dbe1809043b840249c9ec2708a9d6aab9e80044
kross <kross@KROSSOVKA-PC>
2023-10-29 21:34:17 +0300

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace CameraOnIMEI
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public async void TakePhoto()
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+                if (photo != null)
+                {
+                    // save the file into local storage
+                    string localFilePath = Path.Combine(FileSystem.AppDataDirectory, photo.FileName);
+
+                    using Stream sourceStream = await photo.OpenReadAsync();
+                    using FileStream localFileStream = File.OpenWrite(localFilePath);
+
+                    await sourceStream.CopyToAsync(localFileStream);
+                }
+            }
+        }
+
+        private void CameraBtn_Clicked(object sender, EventArgs e)
+        {
+            TakePhoto();
+        }
+    }
+}
+
+
+    
